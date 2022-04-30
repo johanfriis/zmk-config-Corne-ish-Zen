43ffc9a7f5f8c8db373d1bf6b540effa962bc7dd
Johan Friis <johan@slowmail.it>
2022-04-30 13:58:21 +0200

    Create keycodes.h

---
+++ config/keycodes.h
@@ -0,0 +1,97 @@
+#define XXXXXXX &none
+#define _______ &none
+
+#define _LEFT__ &kp LEFT
+#define __UP___ &kp UP
+#define _DOWN__ &kp DOWN
+#define _RIGHT_ &kp RIGHT
+
+#define _SPACE_ &kp SPACE
+#define __TAB__ &kp TAB
+#define _ENTER_ &kp ENTER
+#define _BKSPC_ &kp BACKSPACE
+#define __DEL__ &kp DELETE
+
+#define ___A___ HMRS A
+#define ___B___ &kp B
+#define ___C___ HLSYS C
+#define ___D___ HMLS D
+#define ___E___ HMRG E
+#define ___F___ &kp F
+#define ___G___ &kp G
+#define ___H___ &kp H
+#define ___I___ HLWIN I
+#define ___J___ &kp J
+#define ___K___ &kp K
+#define ___L___ &kp L
+#define ___M___ HMLO M
+#define ___N___ HMLG N
+#define ___O___ &kp O
+#define ___P___ HMLC P
+#define ___R___ &kp R
+#define ___S___ HLNUM S
+#define ___T___ HLNAV T
+#define ___U___ &kp U
+#define ___V___ &kp V
+#define ___W___ &kp W
+#define ___X___ &kp X
+#define ___Y___ &kp Y
+
+#define _ARING_ &kp RA(A)
+#define _OSLSH_ &kp RA(O)
+#define _AELIG_ &kp RA(SINGLE_QUOTE)
+
+#define _COMMA_ &morph_comma_underscore
+#define _MINUS_ &morph_minus_plus
+#define _COLON_ &morph_colon_pipe
+#define _GRAVE_ &morph_grave_equal
+#define __DOT__ &morph_hmrc_dot_ampersand
+#define _SLASH_ &morph_hmro_slash_star
+#define _SQUOT_ &morph_squote_exclamation
+#define _DQUOT_ &morph_dquote_question
+
+#define _LSHFT_ &sk LEFT_SHIFT
+#define _LCTRL_ &sk LEFT_CONTROL
+#define _LOPT__ &sk LEFT_ALT
+#define _LCMD__ &sk LEFT_COMMAND
+
+#define _W_NEXT &kp LG(NON_US_BACKSLASH)
+#define _W_PREV &kp LS(LG(NON_US_BACKSLASH))
+
+#define ___0___ &kp N0
+#define ___1___ &kp N1
+#define ___2___ &kp N2
+#define ___3___ &kp N3
+#define ___4___ &kp N4
+#define ___5___ &kp N5
+#define ___6___ &kp N6
+#define ___7___ &kp N7
+#define ___8___ &kp N8
+#define ___9___ &kp N9
+
+#define o_TABL_ &kp LS(LG(LEFT_BRACKET))
+#define o_TABR_ &kp LS(LG(RIGHT_BRACKET))
+
+#define MAC_UNDO &kp LG(Z)
+#define MAC_REDO &kp LS(LG(Z))
+#define MAC_COPY &kp LG(C)
+#define MAC_PASTE &kp LG(V)
+#define MAC_CUT &kp LG(X)
+#define MAC_SELECT_ALL &kp LG(A)
+
+#define _BRIMN_ &kp F14
+#define _BRIMX_ &kp F15
+
+#define _DSKT1_ &kp LS(LC(LA(LG(N1))))
+#define _DSKT2_ &kp LS(LC(LA(LG(N2))))
+#define _DSKT3_ &kp LS(LC(LA(LG(N3))))
+#define _DSKT4_ &kp LS(LC(LA(LG(N4))))
+#define _DSKT5_ &kp LS(LC(LA(LG(N5))))
+#define _DSKT6_ &kp LS(LC(LA(LG(N6))))
+
+#define _SCRSH_ &kp LS(LG(N4))
+
+#define _BTCLR_ &bt BT_CLR
+#define _BT_00_ &bt BT_SEL 0
+#define _BT_01_ &bt BT_SEL 1
+#define _BT_02_ &bt BT_SEL 2
