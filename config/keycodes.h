3e9ef76c45c6ff55f76565ed49c26095738a7afb
Johan Friis <jfriis@dataminr.com>
2022-08-16 01:59:44 +0200

    try new structure

--- config/keycodes.h
+++ config/keycodes.h
@@ -1,130 +1,98 @@
-#define XXXXXXX &none
-#define _______ &none
+#define X &none
+#define XXXXXXXXXXX &none
+#define ___________ &none
 
+#define __DESKTOP__ SQ(LS(LC(LA(LGUI))))
+#define __WINSWAP__ &kp LG(NON_US_BACKSLASH)
 
-#define _CWORD_ &caps_word
+#define ___TABL____ &kp LS(LG(LEFT_BRACKET))
+#define ___TABR____ &kp LS(LG(RIGHT_BRACKET))
 
-#define _LEFT__ &kp LEFT
-#define __UP___ &kp UP
-#define _DOWN__ &kp DOWN
-#define _RIGHT_ &kp RIGHT
+#define ___UNDO____ &kp LG(Z)
+#define ___REDO____ &kp LS(LG(Z))
+#define ___COPY____ &kp LG(C)
+#define ___PASTE___ &kp LG(V)
+#define ____CUT____ &kp LG(X)
+#define __SELECT___ &kp LG(A)
 
-#define _SPACE_ &kp SPACE
-#define __TAB__ &kp TAB
-#define _ENTER_ &kp ENTER
-#define _BKSPC_ &kp BACKSPACE
-#define __DEL__ &kp DELETE
+#define ___LEFT____ &kp LEFT
+#define ____UP_____ &kp UP
+#define ___DOWN____ &kp DOWN
+#define ___RIGHT___ &kp RIGHT
 
-//#define ___A___ HMRS A
-//#define ___C___ HLSYS C
-//#define ___D___ HMLS D
-//#define ___E___ HMRG E
-//#define ___I___ HLWIN I
-//#define ___M___ HMLO M
-//#define ___N___ HMLG N
-//#define ___P___ HMLC P
-//#define ___S___ HLNUM S
-//#define ___T___ HLNAV T
+#define ___SPACE___ &kp SPACE
+#define ____TAB____ &kp TAB
+#define ___ENTER___ &kp ENTER
+#define ___BKSPC___ &kp BACKSPACE
+#define ____DEL____ &kp DELETE
+#define ____ESC____ &kp ESC
 
-#define ___A___ &kp A
-#define ___B___ &kp B
-#define ___C___ &kp C
-#define ___D___ &kp D
-#define ___E___ &kp E
-#define ___F___ &kp F
-#define ___G___ &kp G
-#define ___H___ &kp H
-#define ___I___ &kp I
-#define ___J___ &kp J
-#define ___K___ &kp K
-#define ___L___ &kp L
-#define ___M___ &kp M
-#define ___N___ &kp N
-#define ___O___ &kp O
-#define ___P___ &kp P
-#define ___Q___ &kp Q
-#define ___R___ &kp R
-#define ___S___ &kp S
-#define ___T___ &kp T
-#define ___U___ &kp U
-#define ___V___ &kp V
-#define ___W___ &kp W
-#define ___X___ &kp X
-#define ___Y___ &kp Y
-#define ___Z___ &kp Z
+#define _____A_____ &kp A
+#define _____B_____ &kp B
+#define _____C_____ &kp C
+#define _____D_____ &kp D
+#define _____E_____ &kp E
+#define _____F_____ &kp F
+#define _____G_____ &kp G
+#define _____H_____ &kp H
+#define _____I_____ &kp I
+#define _____J_____ &kp J
+#define _____K_____ &kp K
+#define _____L_____ &kp L
+#define _____M_____ &kp M
+#define _____N_____ &kp N
+#define _____O_____ &kp O
+#define _____P_____ &kp P
+#define _____Q_____ &kp Q
+#define _____R_____ &kp R
+#define _____S_____ &kp S
+#define _____T_____ &kp T
+#define _____U_____ &kp U
+#define _____V_____ &kp V
+#define _____W_____ &kp W
+#define _____X_____ &kp X
+#define _____Y_____ &kp Y
+#define _____Z_____ &kp Z
 
-#define _ARING_ &kp RA(A)
-#define _OSLSH_ &kp RA(O)
-#define _AELIG_ &kp RA(SINGLE_QUOTE)
+#define ___ARING___ &kp RA(A)
+#define ___OSLSH___ &kp RA(O)
+#define ___AELIG___ &kp RA(SINGLE_QUOTE)
 
-// #define _COMMA_ &morph_comma_underscore
-// #define _MINUS_ &morph_minus_plus
-// #define _COLON_ &morph_colon_pipe
-// #define _GRAVE_ &morph_grave_equal
-// #define __DOT__ &morph_hmrc_dot_ampersand
-// #define _SLASH_ &morph_hmro_slash_star
-// #define _SQUOT_ &morph_squote_exclamation
-// #define _DQUOT_ &morph_dquote_question
-#define _COMMA_ &kp COMMA
-#define _UNDRS_ &kp UNDERSCORE
-#define _MINUS_ &kp MINUS
-#define _PLUS__ &kp PLUS
-#define _COLON_ &kp COLON
-#define __SEMI_ &kp SEMICOLON
-#define _PIPE__ &kp PIPE
-#define _GRAVE_ &kp GRAVE
-#define _EQUAL_ &kp EQUAL
-#define __DOT__ &kp DOT
-#define __AMP__ &kp AMPERSAND
-#define _SLASH_ &kp SLASH
-#define _STAR__ &kp STAR
-#define _SQUOT_ &kp SINGLE_QUOTE
-#define _DQUOT_ &kp DOUBLE_QUOTES
-#define _EXCLM_ &kp EXCLAMATION
-#define _QUEST_ &kp QUESTION
+#define ___COMMA___ &kp COMMA
+#define ___UNDRS___ &kp UNDERSCORE
+#define ___MINUS___ &kp MINUS
+#define ___PLUS____ &kp PLUS
+#define ___COLON___ &kp COLON
+#define ____SEMI___ &kp SEMICOLON
+#define ___PIPE____ &kp PIPE
+#define ___GRAVE___ &kp GRAVE
+#define ___EQUAL___ &kp EQUAL
+#define ____DOT____ &kp DOT
+#define ____AMP____ &kp AMPERSAND
+#define ___SLASH___ &kp SLASH
+#define ___STAR____ &kp STAR
+#define ___SQUOT___ &kp SINGLE_QUOTE
+#define ___DQUOT___ &kp DOUBLE_QUOTES
+#define ___EXCLM___ &kp EXCLAMATION
+#define ___QUEST___ &kp QUESTION
 
-//#define _LSHFT_ &sk LEFT_SHIFT
-#define _LCTRL_ &sk LEFT_CONTROL
-#define _LOPT__ &sk LEFT_ALT
-#define _LCMD__ &sk LEFT_COMMAND
+#define _____0_____ &kp N0
+#define _____1_____ &kp N1
+#define _____2_____ &kp N2
+#define _____3_____ &kp N3
+#define _____4_____ &kp N4
+#define _____5_____ &kp N5
+#define _____6_____ &kp N6
+#define _____7_____ &kp N7
+#define _____8_____ &kp N8
+#define _____9_____ &kp N9
 
-#define _W_NEXT &kp LG(NON_US_BACKSLASH)
-#define _W_PREV &kp LS(LG(NON_US_BACKSLASH))
+#define ___BRIMN___ &kp F14
+#define ___BRIMX___ &kp F15
 
-#define ___0___ &kp N0
-#define ___1___ &kp N1
-#define ___2___ &kp N2
-#define ___3___ &kp N3
-#define ___4___ &kp N4
-#define ___5___ &kp N5
-#define ___6___ &kp N6
-#define ___7___ &kp N7
-#define ___8___ &kp N8
-#define ___9___ &kp N9
+#define ___SCRSH___ &kp LS(LG(N4))
 
-#define o_TABL_ &kp LS(LG(LEFT_BRACKET))
-#define o_TABR_ &kp LS(LG(RIGHT_BRACKET))
-
-#define MAC_UNDO &kp LG(Z)
-#define MAC_REDO &kp LS(LG(Z))
-#define MAC_COPY &kp LG(C)
-#define MAC_PASTE &kp LG(V)
-#define MAC_CUT &kp LG(X)
-#define MAC_SELECT_ALL &kp LG(A)
-
-#define _BRIMN_ &kp F14
-#define _BRIMX_ &kp F15
-
-#define _DSKT1_ &kp LS(LC(LA(LG(N1))))
-#define _DSKT2_ &kp LS(LC(LA(LG(N2))))
-#define _DSKT3_ &kp LS(LC(LA(LG(N3))))
-#define _DSKT4_ &kp LS(LC(LA(LG(N4))))
-#define _DSKT5_ &kp LS(LC(LA(LG(N5))))
-#define _DSKT6_ &kp LS(LC(LA(LG(N6))))
-
-#define _SCRSH_ &kp LS(LG(N4))
-
-#define _BTCLR_ &bt BT_CLR
-#define _BT_00_ &bt BT_SEL 0
-#define _BT_01_ &bt BT_SEL 1
-#define _BT_02_ &bt BT_SEL 2
+#define ___BTCLR___ &bt BT_CLR
+#define ___BT_00___ &bt BT_SEL 0
+#define ___BT_01___ &bt BT_SEL 1
+#define ___BT_02___ &bt BT_SEL 2
