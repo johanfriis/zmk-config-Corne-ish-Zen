d1778e792a1ec07d8ffda71239e29c59cd468eb9
Johan Friis <jfriis@dataminr.com>
2022-08-20 01:17:00 +0200

    try to include board in repo

---
+++ config/boards/arm/zen/board.cmake
@@ -0,0 +1,8 @@
+#
+# Copyright (c) 2021 Darryl deHaan
+# SPDX-License-Identifier: MIT
+#
+
+board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
+
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
